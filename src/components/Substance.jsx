755fb01fdd64983b0d7f9ce5efc394259ceaa385
guipedrov <guilhermepedrov@gmail.com>
2022-06-09 23:22:54 -0300

    Style setting

--- src/components/Substance.jsx
+++ src/components/Substance.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Substance = () => {
   return (
-    <div>Substance</div>
+    <div className="small container">
+      <div>
+
+
+      </div>
+    </div>
   )
 }
 
